59b3e30e3fb1db93d186563cdb64c5c90a8d52c4
tnobile <takao.nosaka@outlook.com>
2021-05-12 00:03:45 +0200

    addback layout

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import fetchFromCMS, { fetchCategoriesFromCMS } from '../lib/graphcmsService';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// export async function getStaticProps() {
-//   const destinos = await fetchFromCMS();
-//   const categories = await fetchCategoriesFromCMS();
-//   return {
-//     props: { destinos, categories },
-//     //revalidate: 30,
-//   };
-// }
+export async function getStaticProps() {
+  const destinos = await fetchFromCMS();
+  const categories = await fetchCategoriesFromCMS();
+  return {
+    props: { destinos, categories },
+    //revalidate: 30,
+  };
+}
 
 export default function Home({ destinos, categories }) {
   console.log("data", destinos);
   return (
+    <Layout categories={categories}>
       <div className='jumbotron jumbotron-fluid'>
         <div className='container text-center m-1'>
           <h1 className='display-2'>Lucerne in 🇨🇭</h1>
           <p1 className='lead'>Excursion ideas/itinesaries</p1></div>
       </div>
+    </Layout>
   );
 }
