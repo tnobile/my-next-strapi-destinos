969a285f6a9bfd0c6ca327d291e9a13375c5229a
tnobile <takao.nosaka@outlook.com>
2021-05-16 00:51:41 +0200

    async await

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ destinos, categories }) {
       <div className='jumbotron jumbotron-fluid'>
         <div className='container text-center m-1'>
           <h1 className='display-2'>❤️ Los Destinos desde Lucerna en🇨🇭❤️</h1>
-          <p1 className='lead'>ideas de excursiones e itinerarios y cosas suizas</p1></div>
+          <p className='lead'>ideas de excursiones e itinerarios y cosas suizas</p></div>
       </div>
       <div className="row justify-content-start ">
         <Destinos destinos={[...destinos].sort((a,b)=> 0.5 - Math.random())} />
