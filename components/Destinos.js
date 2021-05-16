32a88bad1e3b60df53700a4b4359fffcf262f522
tnobile <takao.nosaka@outlook.com>
2021-05-16 16:27:30 +0200

    try with xs

--- components/Destinos.js
+++ components/Destinos.js
@@ -3,8 +3,8 @@ import Destino from "./Destino"
 const Destinos = ({ destinos }) => {
     return (
         destinos && destinos.map(d =>
-            <div className="col-md-3">
-                <div className="mb-3">
+            <div className="col-md-3 col-xs-6">
+                <div className="mb-1">
                     <Destino d={d} key={d.id} />
                 </div>
             </div>
