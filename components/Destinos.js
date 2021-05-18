8bb88150c1381a981cee2797e6d50755c70cbc3a
tnobile <takao.nosaka@outlook.com>
2021-05-18 23:53:59 +0200

    use different grid on top

--- components/Destinos.js
+++ components/Destinos.js
@@ -2,12 +2,16 @@ import Destino from "./Destino"
 
 const Destinos = ({ destinos }) => {
     return (
-        destinos && destinos.map(d =>
-            <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                <Destino d={d} key={d.id} />
-            </div>
-        )
-    )
+        destinos && destinos.map((d, i) =>
+            i < 3 ?
+                < div className="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12" >
+                    <Destino d={d} key={d.id} width={600} height={400} />
+                </div >
+                :
+                < div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12" >
+                    <Destino d={d} key={d.id} />
+                </div >
+        ))
 }
 
 export default Destinos
