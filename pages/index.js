bdd5f7f57e59d8ccef392fa912902cd0f83b7342
tnobile <takao.nosaka@outlook.com>
2021-05-19 19:57:05 +0200

    useInterval

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,25 @@ import Layout from '../components/Layout';
 import fetchFromCMS from '../lib/graphcms-apollo';
 import Destinos from '../components/Destinos';
 import { initializeApollo } from "../lib/apollo-client"
+import { useInterval } from '../hooks/useInterval'
+import { useState } from 'react';
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo();
   const [destinos, categories] = await fetchFromCMS();
   return {
-    props: { destinos, categories,  initialApolloState: apolloClient.cache.extract()},
+    props: { destinos, categories, initialApolloState: apolloClient.cache.extract() },
     revalidate: 1800,
   };
 }
 
 export default function Home({ destinos, categories }) {
-  //console.log("data", destinos);
+  const [list, setList] = useState(destinos);
+
+  useInterval(() => {
+    setList([...list].sort((a, b) => 0.5 - Math.random()));
+  }, 6000)
+
   return (
     <Layout categories={categories}>
       <div className='jumbotron jumbotron-fluid'>
@@ -22,7 +29,7 @@ export default function Home({ destinos, categories }) {
           <p className='lead'>ideas de excursiones e itinerarios y cosas suizas</p></div>
       </div>
       <div className="row justify-content-start text-center ">
-        <Destinos destinos={[...destinos].sort((a,b)=> 0.5 - Math.random())} />
+        <Destinos destinos={list} />
       </div>
     </Layout>
   );
